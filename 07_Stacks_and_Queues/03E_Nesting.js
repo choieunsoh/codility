754019b0617c9af33de461bf60d40ef698f32da4
Kiattipong Kamonrat <kiattipong@thaibma.or.th>
2022-03-13 16:11:04 +0700

    add Nesting solution

---
+++ 07_Stacks_and_Queues/03E_Nesting.js
@@ -0,0 +1,12 @@
+// https://app.codility.com/programmers/lessons/7-stacks_and_queues/nesting/
+
+function solution(S) {
+  if (S.length % 2 > 0) return 0
+  let count = 0
+  for (const s of S) {
+    if (s === '(') count++
+    else if (s === ')') count--
+    if (count < 0) return 0
+  }
+  return count === 0 ? 1 : 0
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# 7. Stacks and Queues
+
+## 3. Nesting
+
+Determine whether a given string of parentheses (single type) is properly nested.
+
+https://app.codility.com/programmers/lessons/7-stacks_and_queues/nesting/
